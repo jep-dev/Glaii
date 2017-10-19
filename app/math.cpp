0f7e86882e7f375397cdbe10030b3850f80bdc89
John Petersen <jepetersen.dev@gmail.com>
2017-10-19 16:39:25 -0400

    Added static assertions and usage examples for geometry types

--- app/math.cpp
+++ app/math.cpp
@@ -14,31 +14,66 @@ using std::setprecision;
 using namespace Geometry;
 
 std::ostream& table(std::ostream& dest) {
-	xyzw<float> x[] = {{0,0,0,1}, {1,0,0,0}, {0,1,0,0}, {0,0,1,0}};
-	unsigned n = 0;
-	for(unsigned i = 0; i < 4; i++) {
-		for(unsigned j = 0; j < 4; j++) {
+	// Quat_t<float> x[] = {{0,0,0,1}, {1,0,0,0}, {0,1,0,0}, {0,0,1,0}};
+	Quat_t<float> x[] = {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0,1}};
+	const std::string sep_str = " | ";
+	const unsigned n = 4, w = 6, sep = sep_str.size();
+	for(unsigned i = 0; i <= n; i++) {
+		std::ostringstream oss;
+		if(i) {
+			oss << x[i-1];
+			dest << ' ' << std::setw(w-2) << oss.str() << ' ';
+		} else {
+			dest << std::setw(w + sep) << "";
+		}
+	}
+	dest << '\n' << std::setw(w+sep-1) << ""
+			<< std::setw(w*4+1) << std::setfill('_')
+			<< "" << std::setfill(' ') << std::endl;
+	for(unsigned i = 0; i < n; i++) {
+		{
+			std::ostringstream oss;
+			oss << x[i];
+			dest << std::setw(w) << oss.str() << sep_str;
+		}
+		for(unsigned j = 0; j < n; j++) {
 			std::ostringstream oss;
 			oss << x[i]*x[j];
-			dest << std::setw(4) << oss.str() << ' ';
-			n++;
+			dest << ' ' << std::setw(w-2) << oss.str() << ' ';
 		}
 		endl(dest);
 	}
-	return dest;
+	return dest << '\n';
 }
 
 int main(int argc, const char *argv[]) {
-	typedef xyzw<float> quat;
-	typedef xyz<float> vert;
-	vert x = {1,0,0}, y = {0,1,0}, z = {0,0,1};
-	quat ident = {0,0,0,1};
+	using std::is_same;
+	Vec_t<float> x = {1,0,0}, y = {0,1,0}, z = {0,0,1};
+	Quat_t<float> ident = {0,0,0,1};
 	table(cout);
 
-	std::map<const char*, quat> xforms = {
-		{"pi/2 . e_i", rotate(float(M_PI/2), x)},
-		{"pi/2 . e_j", rotate(float(M_PI/2), y)},
-		{"pi/2 . e_k", rotate(float(M_PI/2), z)}
+	static_assert(is_same<
+			decltype(rotate(M_PI, x)), Quat_t<double>
+		>::value, "LCM(double, float) should be double");
+	static_assert(is_same<
+			decltype(rotate<float>(M_PI, x)), Quat_t<float>
+		>::value, "LCM(float, float) should be float");
+	static_assert(is_same<
+			decltype(rotate(float(M_PI), x)), Quat_t<float>
+		>::value, "LCM(float(double), float) should be float");
+
+	float f1 = float(M_PI)/512, f2 = float(M_PI/512);
+	int i1 = *reinterpret_cast<int*>(&f1),
+		i2 = *reinterpret_cast<int*>(&f2);
+	cout << "f1 = " << f1 << ", f2 = " << f2 << ";\n"
+		<< "f1-f2 = " << (f1-f2) << ";\n"
+		<< "f1_int = " << i1 << "; f2_int = " << i2 << ";\n"
+		<< "f1_int - f2_int = " << (i1 - i2) << endl;
+
+	std::map<const char*, Quat_t<float>> xforms = {
+		{"pi/2 . e_i", rotate<float>(M_PI/2, x)},
+		{"pi/2 . e_j", rotate<float>(M_PI/2, y)},
+		{"pi/2 . e_k", rotate<float>(M_PI/2, z)}
 	};
 	for(auto const& kv : xforms) {
 		auto const& label = kv.first;
@@ -49,4 +84,5 @@ int main(int argc, const char *argv[]) {
 			cout << "    p = " << p << " --> qpq* = " << q*p << endl;
 		}
 	}
+	endl(cout << "Do integer versions work? " << Quat_t<int>{1, 2, 3, 4});
 }
