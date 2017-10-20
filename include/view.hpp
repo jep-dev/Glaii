7326a21157197d3acc0e0d9aa8a03993b738ba0b
John Petersen <jepetersen.dev@gmail.com>
2017-10-20 14:48:26 -0400

    Added functions to convert between aspect ratios and fields of view, including an experimental parametrized version to ease between vertical-invariant ('hor+') and horizontal-invariant scaling behavior (certainly needs testing.)

--- include/view.hpp
+++ include/view.hpp
@@ -45,6 +45,19 @@ namespace Streams {
 namespace View {
 	using namespace Streams;
 	using namespace gl;
+
+	template<typename T>
+	T fov(T opposite, T aspect) {
+		return 2*atan(tan(opposite/2)*aspect);
+	}
+	template<typename T>
+	T fovEase(T src, T dest, T aspect, T ease = 0.5) {
+		return fov(src, aspect)*ease + fov(dest, aspect)*(1-ease);
+	}
+	template<typename T>
+	T aspectEase(T src, T dest, T aspect, T ease = 0.5) {
+		return tan(fovEase(dest, src, aspect, ease)/2);
+	}
 }
 
 #endif
