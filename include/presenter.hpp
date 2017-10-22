d23308c53853514260526d7fbc3e39c4b2103083
John Petersen <jepetersen.dev@gmail.com>
2017-10-22 03:42:20 -0400

    Added presenter stub as a basic updatable; added comments

--- include/presenter.hpp
+++ include/presenter.hpp
@@ -1,10 +1,19 @@
 #ifndef PRESENTER_HPP
 #define PRESENTER_HPP
 
-#include "geometry.hpp"
+#include "runnable.hpp"
 
 namespace Presenter {
-	//
+	template<typename T>
+	struct Presenter_t;
+	/**
+	 * @brief Presenter type as per model-view-presenter (future Runnable)
+	 * @tparam T The derived class providing the update method
+	 */
+	template<typename T>
+	struct Presenter_t: Abstract::Updatable<T> {
+		using Abstract::Updatable<T>::update;
+	};
 }
 
 #endif
