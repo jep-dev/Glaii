a4e80aa349286a7a8578b32dbcc9fb91854f87db
John Petersen <jepetersen.dev@gmail.com>
2017-10-22 03:41:32 -0400

    Removed commented code and added comments to the rest

--- include/runnable.hpp
+++ include/runnable.hpp
@@ -5,62 +5,46 @@
 
 namespace Abstract {
 	/** @brief Interface for types supporting calls to update through CRTP.
-	 * TODO @tparam T The derived type of the implementation class
+	 * @tparam T The derived type of the implementation class
 	 */
 	template<typename S>
 	struct Updatable: derived<S> {
 	public:
 		typedef typename derived<S>::derived_type derived_type;
 		using derived<S>::get_derived;
-		/*template<typename... T, typename... U, typename... V>
-		static bool update(Updatable<S> const& s,
-				T &... t, U const&... u, V &&... v) {
-			//return static_cast<S *const>(&s)
-			return true;
-			return s.get_derived().
-				update(t..., u..., std::forward<V>(v)...);
-		}*/
-		/*template<typename... T, typename... U, typename... V>
-		static bool update(Updatable<S> &s,
-				T &... t, U const&... u, V &&... v) {
-			return true;
-			//return static_cast<S*>(&s)
-			return s.get_derived().
-				update(t..., u..., std::forward<V>(v)...);
-		}*/
-		/*template<typename S2, typename... T, typename... U, typename... V>
-		static bool update(S2 const& s2,
-				T &... t, U const&... u, V &&... v) {
-			return false;
-		}*/
-		// TODO - superfluous, right?
-		//template<typename S2, typename... T, typename... U, typename... V>
-
-		/*template<typename... T, typename... U, typename... V>
-		static bool update(T &... t, U const&... u, V &&... v) {
-			return false;
-		}*/
-
-		/*template<typename... T, typename... U, typename... V>
-		bool update(T &... t, U const&... u, V &&... v) const {
-			return static_cast<S *const>(this)
-				-> update(t..., u..., std::forward<V>(v)...);
-		}
-		template<typename... T, typename... U, typename... V>
-		bool update(T &... t, U const&... u, V &&... v) {
-			return static_cast<S*>(this)
-				-> update(t..., u..., std::forward<V>(v)...);
-		}*/
 	};
 }
 
-
+/**
+ * @brief Updates the given constant subject using its derived implementation
+ * @tparam S The class providing the derived implementation of update
+ * @tparam T The types of the referenced arguments
+ * @tparam U The types of the referenced constant arguments
+ * @tparam V The types of the arguments given by value
+ * @param s The subject which should be updated
+ * @param t The referenced arguments
+ * @param u the referenced constant arguments
+ * @param v The arguments given by value
+ * @return True if and only if the derived implementation returns true
+ */
 template<typename S, typename... T, typename... U, typename... V>
 bool update(Abstract::Updatable<S> const& s,
 		T &... t, U const&... u, V &&... v) {
 	return static_cast<S *const>(&s)
 		-> update(t..., u..., std::forward<V>(v)...);
 }
+/**
+ * @brief Updates the given subject using its derived implementation
+ * @tparam S The class providing the derived implementation of update
+ * @tparam T The types of the referenced arguments
+ * @tparam U The types of the referenced constant arguments
+ * @tparam V The types of the arguments given by value
+ * @param s The subject which should be updated
+ * @param t The referenced arguments
+ * @param u the referenced constant arguments
+ * @param v The arguments given by value
+ * @return True if and only if the derived implementation returns true
+ */
 template<typename S, typename... T, typename... U, typename... V>
 bool update(Abstract::Updatable<S>& s, T &... t, U const&... u, V &&... v) {
 	return static_cast<S*>(&s)
