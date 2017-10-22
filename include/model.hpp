d412ab04b2bf530d464d4493a4ccd24549738f0b
John Petersen <jepetersen.dev@gmail.com>
2017-10-22 03:42:32 -0400

    Added comments to model stub

--- include/model.hpp
+++ include/model.hpp
@@ -9,17 +9,21 @@ namespace Model {
 	template<typename>
 	struct ModelBase;
 
+	/**
+	 * @brief Abstract Updatable model type (future Runnable)
+	 * @tparam T The derived class providing the update method
+	 */
 	template<typename T>
 	struct ModelBase: Abstract::Updatable<T> {
 		using Abstract::Updatable<T>::update;
 	};
 
-	template<typename S, unsigned BRANCH = 0>
-	struct Octree {
+	/*template<typename S, unsigned BRANCH = 0>
+	struct Octree { // TODO
 		typedef typename std::conditional<BRANCH,
 				Octree<S, BRANCH - 1>[8], S>::type data_type;
 		data_type data;
-	};
+	};*/
 
 }
 
