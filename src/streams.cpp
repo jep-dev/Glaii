110fd1f27cc21f265b9182e6045017d1561db83e
John Petersen <jepetersen.dev@gmail.com>
2017-10-27 09:38:51 -0400

    Formatting; removed shorthand for istreambuf_iterator (use of default constructor drops named use count to 1)

--- src/streams.cpp
+++ src/streams.cpp
@@ -1,15 +1,19 @@
 #include "streams.hpp"
 
 namespace Streams {
-	Source::operator const string(void) const
-		{ return data; }
-	Source::operator const char*(void) const
-		{ return data.c_str(); }
+	Source::operator string const&(void) const {
+		return data;
+	}
+	Source::operator const char*(void) const {
+		return data.c_str();
+	}
 
 	Source::Source(void) {}
 	Source::Source(string const& s): data(s) {}
 	Source::Source(string && s): data(std::move(s)) {}
 	template<typename S>
-	Source::Source(S s): data((isbuf_it(s)), isbuf_it()) {}
-	Source::Source(const char *fname): Source(ifstream(fname)) {}
+	Source::Source(S && s):
+		data(std::istreambuf_iterator<char>(s), {}) {}
+	Source::Source(const char *fname):
+		Source(ifstream(fname)) {}
 }
