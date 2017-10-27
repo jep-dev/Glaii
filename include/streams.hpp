e7e05114f0d99aa6cbaac9907512d6b9e08e59d3
John Petersen <jepetersen.dev@gmail.com>
2017-10-27 12:30:04 -0400

    Added more explicit constructors from streambufs; possibly redundant validation step ('good' method called as predicate to iterator; fallback to default iterator constructor, s.t. data is empty)

--- include/streams.hpp
+++ include/streams.hpp
@@ -15,18 +15,35 @@ namespace Streams {
 	using std::string;
 	using std::ostream;
 	using std::ifstream;
+	using std::basic_streambuf;
 	using std::ostringstream;
 
 	/** @brief Reads entire ifstream into memory. */
 	struct Cutter {
+		template<typename T>
+		using BufIterator_t = std::istreambuf_iterator<T>;
+
 		const string data;
+		operator bool(void) const;
 		operator const char*(void) const;
 		operator string const&(void) const;
+
 		Cutter(void);
-		Cutter(string && s);
-		Cutter(string const& s);
+		Cutter(string && src);
+		Cutter(string const& src);
 		Cutter(const char *fname);
-		template<typename S> Cutter(S && s);
+
+		template<typename T>
+		Cutter(BufIterator_t<T> && p0, BufIterator_t<T> && p1 = {}):
+			data(p0, p1) {}
+
+		template<typename S>
+		Cutter(S& s): data((s.good() ?
+				BufIterator_t<char>(s) : BufIterator_t<char>{}), {}) {}
+		template<typename S>
+		Cutter(S && s): data((s.good() ?
+				BufIterator_t<char>(s) : BufIterator_t<char>{}), {}) {}
+			//data(std::istreambuf_iterator<char>(s), {}) {}
 	};
 
 	struct Paster {
