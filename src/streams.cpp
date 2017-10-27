a6c880baf256e1b7ef7ff578a997aa3f75f3cbcd
John Petersen <jepetersen.dev@gmail.com>
2017-10-27 12:31:53 -0400

    Added boolean cast operator as alias for non-empty data; remove void constructor?

--- src/streams.cpp
+++ src/streams.cpp
@@ -1,19 +1,18 @@
 #include "streams.hpp"
 
 namespace Streams {
+	Cutter::operator bool(void) const {
+		return data.size();
+	}
 	Cutter::operator string const&(void) const {
 		return data;
 	}
 	Cutter::operator const char*(void) const {
 		return data.c_str();
 	}
-
 	Cutter::Cutter(void) {}
 	Cutter::Cutter(string const& s): data(s) {}
 	Cutter::Cutter(string && s): data(std::move(s)) {}
-	template<typename S>
-	Cutter::Cutter(S && s):
-		data(std::istreambuf_iterator<char>(s), {}) {}
 	Cutter::Cutter(const char *fname):
 		Cutter(ifstream(fname)) {}
 }
