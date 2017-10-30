70e3f32664eed7fb2bd2762d8161d978f050d4e6
John Petersen <jepetersen.dev@gmail.com>
2017-10-30 05:15:41 -0400

    Moved non-template methods off the header

--- include/paster.hpp
+++ include/paster.hpp
@@ -28,41 +28,9 @@ namespace Streams {
 			oss << *this;
 			return oss.str();
 		}
-		Paster& flush(size_t toCol = 0, size_t toRow = 0) {
-			toCol = std::accumulate(data.cbegin(), data.cend(), toCol,
-				[] (size_t lhs, string const& rhs) -> size_t {
-					return std::max(lhs, rhs.size());
-				});
-			std::for_each(data.begin(), data.end(),
-				[=] (string& src) -> string& {
-				if(toCol > src.size())
-					return src += string(toCol-src.size(), pad);
-				});
-			while(data.size() < toRow)
-				data.emplace_back(toCol, pad);
-			return *this;
-		}
-		Paster& operator<<(string const& rhs) {
-			std::vector<string> sp;
-			std::istringstream ss(rhs);
-			while(ss) {
-				string buf;
-				if(getline(ss, buf, '\n')) {
-					sp.emplace_back(buf);
-				}
-			}
-			flush(0, sp.size());
-			auto dBeg = data.begin();
-			for(auto const& el : sp) {
-				if(dBeg == data.end()) break;
-				*dBeg += el;
-				dBeg++;
-			}
-			return flush();
-		}
-		Paster& operator<<(ostringstream const& rhs) {
-			return (*this) << rhs.str();
-		}
+		Paster& flush(size_t toCol = 0, size_t toRow = 0);
+		Paster& operator<<(string const& rhs);
+		Paster& operator<<(ostringstream const& rhs);
 		template<typename... T>
 		Paster& column(T const&... t) {
 			ostringstream oss;
@@ -91,9 +59,7 @@ namespace Streams {
 				dest << t << '\n';
 			return repeat(dest, tn...);
 		}
-		Paster& repeat(ostringstream& dest) {
-			return *this << dest;
-		}
+		Paster& repeat(ostringstream& dest);
 		template<typename T>
 		Paster& center(T const& t) {
 			ostringstream oss;
--- src/paster.cpp
+++ src/paster.cpp
@@ -1,2 +1,44 @@
 #include "streams.hpp"
 #include "paster.hpp"
+
+namespace Streams {
+
+		Paster& Paster::flush(size_t toCol, size_t toRow) {
+			toCol = std::accumulate(data.cbegin(), data.cend(), toCol,
+				[] (size_t lhs, string const& rhs) -> size_t {
+					return std::max(lhs, rhs.size());
+				});
+			std::for_each(data.begin(), data.end(),
+				[=] (string& src) -> string& {
+				if(toCol > src.size())
+					return src += string(toCol-src.size(), pad);
+				});
+			while(data.size() < toRow)
+				data.emplace_back(toCol, pad);
+			return *this;
+		}
+		Paster& Paster::operator<<(string const& rhs) {
+			std::vector<string> sp;
+			std::istringstream ss(rhs);
+			while(ss) {
+				string buf;
+				if(getline(ss, buf, '\n')) {
+					sp.emplace_back(buf);
+				}
+			}
+			flush(0, sp.size());
+			auto dBeg = data.begin();
+			for(auto const& el : sp) {
+				if(dBeg == data.end()) break;
+				*dBeg += el;
+				dBeg++;
+			}
+			return flush();
+		}
+		Paster& Paster::operator<<(ostringstream const& rhs) {
+			return (*this) << rhs.str();
+		}
+		Paster& Paster::repeat(ostringstream& dest) {
+			return *this << dest;
+		}
+}
