419c1113e4b658754a1754ef76157c12155cea75
John Petersen <jepetersen.dev@gmail.com>
2017-12-03 00:09:23 -0500

    Moved the real coordinate to the head of the polynomial from the tail

--- include/quaternion.hpp
+++ include/quaternion.hpp
@@ -10,30 +10,21 @@ namespace Geometry {
 	 */
 	template<typename X>
 	struct Quat_t {
-		X x, y, z, w;
+		X w, x, y, z;
 		template<typename R>
 		Quat_t<X> operator=(Quat_t<R> const& r) {
-			x = X(r.x); y = X(r.y);
-			z = X(r.z); w = X(r.w);
+			w = X(r.w); x = X(r.x);
+			y = X(r.y); z = X(r.z);
 			return *this;
 		}
 		Quat_t<X> operator*(void) const {
-			return {-x, -y, -z, w};
+			return {w, -x, -y, -z};
 		}
 		Quat_t<X> operator-(void) const {
-			return {-x, -y, -z, -w};
-		}
-		X operator[](unsigned char r) const {
-			switch(r) {
-				case 'i': case 'x': return x;
-				case 'j': case 'y': return y;
-				case 'k': case 'z': return z;
-				case '1': case 'w': return w;
-				default: return 0;
-			}
+			return {-w, -x, -y, -z};
 		}
 		X magnitude2(void) const {
-			return x*x + y*y + z*z + w*w;
+			return w*w + x*x + y*y + z*z;
 		}
 		X magnitude(void) const {
 			return X(sqrt(magnitude2()));
@@ -43,20 +34,20 @@ namespace Geometry {
 		}
 		template<typename R, typename XR = COMBINE(X,+,R)>
 		Quat_t<XR> operator+(Quat_t<R> const& r) const {
-			return {XR(x+r.x), XR(y+r.y), XR(z+r.z), XR(w+r.w)};
+			return {XR(w+r.w), XR(x+r.x), XR(y+r.y), XR(z+r.z)};
 		}
 		template<typename R, typename XR = COMBINE(X,-,R)>
 		Quat_t<XR> operator-(Quat_t<R> const& r) const {
-			return {XR(x-r.x), XR(y-r.y), XR(z-r.z), XR(w-r.w)};
+			return {XR(w-r.w), XR(x-r.x), XR(y-r.y), XR(z-r.z)};
 		}
 		template<typename R>
 		bool operator==(Quat_t<R> const& r) const {
-			return x == r.x && y == r.y
-				&& z == r.z && w == r.w;
+			return w == r.w && x == r.x
+				&& y == r.y && z == r.z;
 		}
 		template<typename R, typename XR = COMBINE(X,/,R)>
 		Quat_t<XR> operator/(R const& r) const {
-			return {x/r, y/r, z/r, w/r};
+			return {w/r, x/r, y/r, z/r};
 		}
 	};
 	/**
@@ -71,30 +62,30 @@ namespace Geometry {
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
 	Quat_t<LR> rotate(L const& l, Vec_t<R> const& r) {
 		LR l2 = LR(l)/2, lc = LR(cos(l2)), ls = LR(sin(l2));
-		return Quat_t<LR>{ls*r.x, ls*r.y, ls*r.z, lc};
+		return Quat_t<LR>{lc, ls*r.x, ls*r.y, ls*r.z};
 	}
 
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
 	Quat_t<LR> operator*(Quat_t<L> const& l, Quat_t<R> const& r) {
 		return {
+			LR(l.w * r.w - l.x * r.x - l.y * r.y - l.z * r.z),
 			LR(l.w * r.x + l.x * r.w + l.y * r.z - l.z * r.y),
 			LR(l.w * r.y - l.x * r.z + l.y * r.w + l.z * r.x),
-			LR(l.w * r.z + l.x * r.y - l.y * r.x + l.z * r.w),
-			LR(l.w * r.w - l.x * r.x - l.y * r.y - l.z * r.z)
+			LR(l.w * r.z + l.x * r.y - l.y * r.x + l.z * r.w)
 		};
 	}
-	/*template<typename L, typename R, typename LR = COMBINE(L,*,R)>
+	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
 	Quat_t<LR> operator*(Quat_t<L> const& l, Vec_t<R> const& r) {
 		return Quat_t<LR> {
+			LR(          - l.x * r.x - l.y * r.y - l.z * r.z),
 			LR(l.w * r.x             + l.y * r.z - l.z * r.y),
 			LR(l.w * r.y - l.x * r.z             + l.z * r.x),
-			LR(l.w * r.z + l.x * r.y - l.y * r.x            ),
-			LR(          - l.x * r.x - l.y * r.y - l.z * r.z)
+			LR(l.w * r.z + l.x * r.y - l.y * r.x            )
 		}**l;
-	}*/
+	}
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
 	Quat_t<LR> operator*(Quat_t<R> const& l, R const& r) {
-		return {LR(l.x*r), LR(l.y*r), LR(l.z*r), LR(l.w*r)};
+		return {LR(l.w*r), LR(l.x*r), LR(l.y*r), LR(l.z*r)};
 	}
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
 	Quat_t<LR> operator*(L const& l, Quat_t<R> const& r) {
