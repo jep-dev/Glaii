47d7618f5318e3486964fbeaddecd5090a214b09
John Petersen <jepetersen.dev@gmail.com>
2017-12-13 02:04:17 -0500

    Added 'repeat' type to reify repetition, as with 'column' type (supporting stream insertion into orthogonal types)

--- include/paster.hpp
+++ include/paster.hpp
@@ -2,6 +2,28 @@
 #define PASTER_HPP
 
 namespace Streams {
+
+	ostringstream repeat(ostringstream && dest) {
+		return std::move(dest);
+	}
+	template<typename T0, typename N = std::size_t, typename... TN>
+	ostringstream repeat(ostringstream && dest,
+			T0 const& t, N repeats, TN const&... tn) {
+		for(auto i = 0; i < repeats; i++)
+			dest << t << '\n';
+		return repeat(std::move(dest), tn...);
+	}
+	template<typename T0, typename... TN>
+	ostringstream repeat(ostringstream && dest,
+			T0 const& t, TN const&... tn) {
+		dest << t;
+		return repeat(std::move(dest), tn...);
+	}
+	template<typename T0, typename N = std::size_t, typename... TN>
+	ostringstream repeat(T0 const& t, N repeats, TN const&... tn) {
+		return repeat(ostringstream(), t, repeats, tn...);
+	}
+
 	template<typename... TN>
 	ostringstream column(ostringstream && oss, TN const&... tn) {
 		return std::move(oss);
@@ -27,6 +49,32 @@ namespace Streams {
 			ostringstream oss;
 			oss << *this;
 			return oss.str();
+		}
+		bool flushed(void) const {
+			unsigned len = 0, cur = 0;
+			bool once = true;
+			for(auto const& line : data)
+				if(len < (cur = line.size())) {
+					if(!once) return false;
+					else len = cur;
+				}
+			return true;
+		}
+		// TODO DRY; replace flushed, widest with pair{least,most}?
+		// Computationally, flushed <= widest <= (non-const) flush
+		unsigned widest(void) const {
+			unsigned len = 0, cur = 0;
+			for(auto const& line : data) {
+				if(len < (cur = line.size()))
+					len = cur;
+			}
+			return len;
+		}
+		std::size_t size(void) const {
+			return data.size();
+		}
+		string operator[](unsigned i) const {
+			return i < size() ? data[i] : "";
 		}
 		Paster& flush(size_t toCol = 0, size_t toRow = 0);
 		Paster& operator<<(string const& rhs);
@@ -83,11 +131,41 @@ namespace Streams {
 			return dest;
 		}
 		Paster(void) {}
+		Paster(Paster const& p): data(p.data) {}
 		template<typename... T>
 		Paster(T const&... t): Paster() {
 			*this << string(t...);
 		}
 	};
+
+	template<typename S>
+	S& border(S &dest, Paster const& src,
+			bool n = true, bool e = true,
+			bool s = true, bool w = true) {
+		Paster p = src;
+		if(!p.flushed()) p.flush();
+		unsigned width = p[0].size(), height = p.size();
+		auto filler = string(width, '-');
+		if(n) {
+			if(w) dest << '.';
+			dest << filler;
+			if(e) dest << '.';
+			dest << '\n';
+		}
+		for(unsigned i = 0; i < height; i++) {
+			if(w) dest << '|';
+			dest << p[i];
+			if(e) dest << '|';
+			dest << '\n';
+		}
+		if(s) {
+			if(w) dest << '\'';
+			dest << filler;
+			if(e) dest << '\'';
+			dest << "\n";
+		}
+		return dest;
+	}
 }
 
 #endif
