677ae6a30d43fc4cf5b7b0dab2c22f69e2591379
John Petersen <jepetersen.dev@gmail.com>
2017-12-13 01:59:52 -0500

    Added conjugacy/sandwich operator, as with quaternion

--- include/dual_quaternion.hpp
+++ include/dual_quaternion.hpp
@@ -8,6 +8,9 @@ namespace Geometry {
 	template<typename X>
 	struct DualQuat_t {
 		Quat_t<X> u = {0,0,0,0}, v = {0,0,0,0};
+		Quat_t<X> operator[](unsigned i) const {
+			return i ? v : u;
+		}
 		template<typename R>
 		DualQuat_t<X> operator=(DualQuat_t<R> const& r) {
 			u = r.u; v = r.v;
@@ -28,14 +31,6 @@ namespace Geometry {
 		DualQuat_t<X> normalize(void) const {
 			return *this / magnitude();
 		}
-		/*template<typename R, typename XR = COMBINE(X,*,R)>
-		DualQuat_t<XR> operator*(DualQuat_t<R> const& r) const {
-			return { u * r.u, v * r.u + u * r.v };
-		}
-		template<typename R, typename XR = COMBINE(X,*,R)>
-		DualQuat_t<XR> operator*(R const& r) const {
-			return {r * u, r * v};
-		}*/
 		template<typename R, typename XR = COMBINE(X,+,R)>
 		DualQuat_t<XR> operator+(DualQuat_t<R> const& r) const {
 			return {u + r.u, v + r.v};
@@ -63,21 +58,17 @@ namespace Geometry {
 		return {l.u*r, l.v*r};
 	}
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	DualQuat_t<LR> operator*(DualQuat_t<L> const& l, R const& r) {
-		return {r*l.u, r*l.v};
-	}
-	/*template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	DualQuat_t<LR> operator*(DualQuat_t<L> const& l, Quat_t<R> const& r) {
-		return {l.u*r, l.v*r};
+	DualQuat_t<LR> operator*(L const& l, DualQuat_t<R> const& r) {
+		return {l*r.u, l*r.v};
 	}
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	DualQuat_t<LR> operator*(DualQuat_t<L> const& l, R const& r) {
-		return {r*l.u, r*l.v};
-	}*/
-	/*template<typename X, typename L, typename XL = COMBINE(L,*,X)>
-	DualQuat_t<XL> operator*(L const& l, DualQuat_t<X> const& r) {
-		return r * l;
-	}*/
+	DualQuat_t<LR> operator^(DualQuat_t<L> const& l, DualQuat_t<R> const& r) {
+		return DualQuat_t<LR>{l.u*r.u, l.u*r.v + l.v*r.u} * *l;
+	}
+	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
+	DualQuat_t<LR> operator^(DualQuat_t<L> const& l, Quat_t<R> const& r) {
+		return DualQuat_t<LR>{l.u*r, l.v*r} * *l;
+	}
 
 	template struct DualQuat_t<float>;
 	template struct DualQuat_t<double>;
