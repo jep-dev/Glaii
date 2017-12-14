cfc5ed38e63b92577727d01cfe9032a2e0e451e9
John Petersen <jepetersen.dev@gmail.com>
2017-12-14 15:27:29 -0500

    Converted to declaration-only header; added generalized magnitude and normal functions dependent only on the dot product instantiated for the operand(s).

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -11,104 +11,40 @@
 
 namespace Geometry {
 
-	template<typename> struct Quat_t;
-	template<typename> struct Vec_t;
-	template<typename> struct DualQuat_t;
-
-	/**
-	 * @brief Quaternion template with format i, j, k, real.
-	 * @tparam X The domain of each member.
-	 */
-	/*
-	template<typename X>
-	struct Quat_t {
-		X x, y, z, w;
-		template<typename R>
-		Quat_t<X> operator=(Quat_t<R> const& r) {
-			x = X(r.x); y = X(r.y);
-			z = X(r.z); w = X(r.w);
-			return *this;
-		}
-		Quat_t<X> operator*(void) const {
-			return {-x, -y, -z, w};
-		}
-		Quat_t<X> operator-(void) const {
-			return {-x, -y, -z, -w};
-		}
-		X operator[](unsigned char r) const {
-			switch(r) {
-				case 'i': case 'x': return x;
-				case 'j': case 'y': return y;
-				case 'k': case 'z': return z;
-				case '1': case 'w': return w;
-				default: return 0;
-			}
-		}
-		X magnitude2(void) const {
-			return x*x + y*y + z*z + w*w;
-		}
-		X magnitude(void) const {
-			return X(sqrt(magnitude2()));
-		}
-		Quat_t<X> normalize(void) const {
-			return *this / magnitude();
-		}
-		template<typename R, typename XR = COMBINE(X,+,R)>
-		Quat_t<XR> operator+(Quat_t<R> const& r) const {
-			return {XR(x+r.x), XR(y+r.y), XR(z+r.z), XR(w+r.w)};
-		}
-		template<typename R, typename XR = COMBINE(X,-,R)>
-		Quat_t<XR> operator-(Quat_t<R> const& r) const {
-			return {XR(x-r.x), XR(y-r.y), XR(z-r.z), XR(w-r.w)};
-		}
-		template<typename R>
-		bool operator==(Quat_t<R> const& r) const {
-			return x == r.x && y == r.y
-				&& z == r.z && w == r.w;
-		}
-		template<typename R, typename XR = COMBINE(X,/,R)>
-		Quat_t<XR> operator/(R const& r) const {
-			return {x/r, y/r, z/r, w/r};
-		}
-	}; */
-
 	/**
 	 * @brief A 3D vector template, analogous to pure-imaginary quaternions.
 	 * @tparam X The domain of each of the members.
 	 */
+	template<typename> struct Vec_t;
+
+	template<typename> struct Quat_t;
+	template<typename> struct DualQuat_t;
+
 	template<typename X>
 	struct Vec_t {
 		X x, y, z;
-		X operator[](unsigned char r) const {
-			switch(r) {
-				case 'i': case 'x': return x;
-				case 'j': case 'y': return y;
-				case 'k': case 'z': return z;
-				default: return 0;
-			}
-		}
-		X magnitude2(void) const {
-			return x*x + y*y + z*z;
-		}
-		X magnitude(void) const {
-			return X(sqrt(magnitude2()));
-		}
-		Vec_t<X> normalize(void) const {
-			return *this / magnitude();
-		}
-		template<typename R, typename XR = COMBINE(X,-,R)>
-		Vec_t<XR> operator-(Vec_t<R> const& r) const {
-			return {x-r.x, y-r.y, z-r.z};
-		}
+		X operator[](unsigned char r) const;
+
+		//X magnitude2(void) const;
+		//X magnitude(void) const;
+		//Vec_t<X> normalize(void) const;
+
 		template<typename R>
-		bool operator==(Vec_t<R> const& r) const {
-			return x == r.x && y == r.y && z == r.z;
-		}
+		bool operator==(Vec_t<R> const& r) const;
+
+		template<typename R, typename XR = COMBINE(X,-,R)>
+		Vec_t<XR> operator-(Vec_t<R> const& r) const;
+
 		template<typename R, typename XR = COMBINE(X,/,R)>
-		Vec_t<XR> operator/(R const& r) const {
-			return {x/r, y/r, z/r};
-		}
+		Vec_t<XR> operator/(R const& r) const;
 	};
+
+	template<typename X>
+	X magnitude2(X const& x);
+	template<typename X>
+	X magnitude(X const& x);
+	template<typename X>
+	X normalize(X const& x);
 
 	/**
 	 * @brief The cross product of two (un-normalized) vector differences.
@@ -120,71 +56,28 @@ namespace Geometry {
 	 * @tparam RST The range of the final cross product
 	 * @return The resulting cross product
 	 */
-	template<typename R, typename S, typename T,
+	template<typename R, typename S = R, typename T = S,
 		typename RS = COMBINE(R,-,S), typename ST = COMBINE(T,-,S),
 		typename RST = COMBINE(RS,*,ST)>
-	Vec_t<RST> cross(Vec_t<R> const& l,
-			Vec_t<S> const& c, Vec_t<T> const& r) {
-		return (l-c)*(r-c);
-	}
-
+	Vec_t<RST> cross(Vec_t<R> const& l, Vec_t<S> const& c, Vec_t<T> const& r);
 
 	/**
-	 * @brief Angle-axis rotation to equivalent quaternion
-	 * @tparam L The domain of the rotation magnitude
-	 * @tparam R The domain of the rotation axis
-	 * @param l The rotation angle in radians
-	 * @param r The rotation axis, generally normalized
-	 * @tparam LR The range of the resulting quaternion
-	 * @return The quaternion representation of the rotation
+	 * @brief The dot or inner product of two vectors.
+	 * @tparam R The domain of the first vector
+	 * @tparam S The domain of the second vector
+	 * @tparam RS The range of the scalar product
+	 * @return The resulting dot/inner product
 	 */
-	/*template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Quat_t<LR> rotate(L const& l, Vec_t<R> const& r) {
-		LR l2 = LR(l)/2, lc = LR(cos(l2)), ls = LR(sin(l2));
-		return Quat_t<LR>{ls*r.x, ls*r.y, ls*r.z, lc};
-	}
-
+	template<typename L, typename R = L,
+		typename LR = COMBINE(L,*,R)>
+	Vec_t<LR> dot(Vec_t<L> const& l, Vec_t<R> const& r);
 
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Quat_t<LR> operator*(Quat_t<L> const& l, Quat_t<R> const& r) {
-		return {
-			LR(l.w * r.x + l.x * r.w + l.y * r.z - l.z * r.y),
-			LR(l.w * r.y - l.x * r.z + l.y * r.w + l.z * r.x),
-			LR(l.w * r.z + l.x * r.y - l.y * r.x + l.z * r.w),
-			LR(l.w * r.w - l.x * r.x - l.y * r.y - l.z * r.z)
-		};
-	}
+	Vec_t<LR> operator*(Vec_t<L> const& l, Vec_t<R> const& r);
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Quat_t<LR> operator*(Quat_t<L> const& l, Vec_t<R> const& r) {
-		return Quat_t<LR> {
-			LR(l.w * r.x             + l.y * r.z - l.z * r.y),
-			LR(l.w * r.y - l.x * r.z             + l.z * r.x),
-			LR(l.w * r.z + l.x * r.y - l.y * r.x            ),
-			LR(          - l.x * r.x - l.y * r.y - l.z * r.z)
-		}**l;
-	}
+	Vec_t<LR> operator*(Vec_t<R> const& l, L const& r);
 	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Quat_t<LR> operator*(Quat_t<R> const& l, R const& r) {
-		return {LR(l.x*r), LR(l.y*r), LR(l.z*r), LR(l.w*r)};
-	}
-	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Quat_t<LR> operator*(L const& l, Quat_t<R> const& r) {
-		return r*l;
-	}*/
-	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Vec_t<LR> operator*(Vec_t<L> const& l, Vec_t<R> const& r) {
-		return {LR(l.y * r.z - l.z * r.y),
-			LR(l.z * r.x - l.x * r.z),
-			LR(l.x * r.y - l.y * r.x)};
-	}
-	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Vec_t<LR> operator*(Vec_t<R> const& l, L const& r) {
-		return {l.x*r, l.y*r, l.z*r};
-	}
-	template<typename L, typename R, typename LR = COMBINE(L,*,R)>
-	Vec_t<LR> operator*(L const& l, Vec_t<R> const& r) {
-		return r * l;
-	}
+	Vec_t<LR> operator*(L const& l, Vec_t<R> const& r);
 
 	/**
 	 * @brief Fuzzy comparison to zero (avoids division due to aliasing)
@@ -195,10 +88,7 @@ namespace Geometry {
 	 * multiplication with the given or default proximity
 	 */
 	template<typename L>
-	bool nearZero(L const& l, unsigned prox = TOLERANCE) {
-		auto lp = l * prox;
-		return lp >= -1 && lp <= 1;
-	}
+	bool nearZero(L const& l, unsigned prox = TOLERANCE);
 	/**
 	 * @brief Rounds to zero by fuzzy comparison
 	 * @tparam T The domain of the value to round
@@ -207,9 +97,7 @@ namespace Geometry {
 	 * @return A copy of t, or zero if t is within prox of zero
 	 */
 	template<typename T>
-	T roundNearZero(T const& t, unsigned prox = TOLERANCE) {
-		return nearZero(t, prox) ? T(0) : t;
-	}
+	T roundNearZero(T const& t, unsigned prox = TOLERANCE);
 	/**
 	 * @brief Fuzzy comparison of multidimensional values
 	 * @tparam L Type of the subject with magnitude method
@@ -221,14 +109,13 @@ namespace Geometry {
 	 * inputs is near zero
 	 */
 	template<typename L, typename R>
-	bool near(L const& l, R const& r, unsigned prox = TOLERANCE) {
-		return nearZero((r-l).magnitude(), prox);
-	}
+	bool near(L const& l, R const& r, unsigned prox = TOLERANCE);
 
-	// Macros if this starts to get tedious
+	// Macros if this starts to get tedious?
 	template struct Vec_t<float>;
 	template struct Vec_t<double>;
 
 }
+#include "geometry.tpp"
 
 #endif
