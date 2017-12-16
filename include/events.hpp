ec13ef03fcce034da43880fd1fee00bcd77cc581
John Petersen <jepetersen.dev@gmail.com>
2017-12-16 04:07:25 -0500

    Added documentation for event handlers; the handler template is still a stub - ideally it should identify a sequence of sub-handlers and/or a sequence of handled events.

--- include/events.hpp
+++ include/events.hpp
@@ -5,20 +5,45 @@
 #include <SDL2/SDL_events.h>
 
 namespace Abstract {
-	template<typename D>
-	struct Handler_t;
+	/**
+	 * @brief Identifies D as a base with the desired handler implementation.
+	 * @tparam D The targeted type; passes unhandled events implicitly.
+	 */
 	template<typename D>
 	struct Handler_t {};
 
+	/**
+	 * @brief Policy for handlers unprepared for the given event.
+	 * @tparam D The type expected to handle the event
+	 * @tparam EV The type of the event to handle
+	 * @param hnd The handler: not type D or missing handle(EV)
+	 * @return True, vacuously - handle(EV) must be defined to fail.
+	 */
 	template<typename D, typename EV>
 	bool call_handler(Handler_t<D>& hnd, EV const& ev, long) {
 		return true;
 	}
+
+	/**
+	 * @brief Passes an event to the (projected) handler.
+	 * @tparam D The type known and designated to handle the event
+	 * @tparam EV The type of the event to handle
+	 * @param hnd The handler instance intended to receive the event.
+	 * @return The type returned by the projected type's
+	 * corresponding method.
+	 */
 	template<typename D, typename EV>
 	auto call_handler(Handler_t<D>& hnd, EV const& ev, int)
 	-> decltype(static_cast<D&>(hnd).handle(ev)) {
 		return static_cast<D&>(hnd).handle(ev);
 	}
+	/**
+	 * @brief Separates event components since this is almost certainly
+	 * not the responsibility of the handler.
+	 * @tparam D The handler's implementation type
+	 * @param hnd The instance given to handle event components
+	 * @param ev The event as its base, undifferentiated 'events'
+	 */
 	template<typename D>
 	bool call_handler(Handler_t<D>& hnd, SDL_Event const& ev) {
 		switch(ev.type) {
